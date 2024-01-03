d726542dfa39ee9e49808ac6e3bc68e5867e3d19
sanghyun27 <rhffoa207@naver.com>
2024-01-03 09:18:46 +0900

    9시 18분

--- src/main/java/com/study/ch04/Operation07.java
+++ src/main/java/com/study/ch04/Operation07.java
@@ -4,5 +4,6 @@ public class Operation07 {
     public static void main(String[] args) {
         System.out.println("깃, 깃허브 수업 실습");
         System.out.println("1월 3일 오전 9시 6분");
+        System.out.println("1월 3일 오전 9시 18분");
     }
 }
