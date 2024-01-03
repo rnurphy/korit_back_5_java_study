117279b0731402c5eb4cf66b09371f518dcf0b7b
sanghyun27 <rhffoa207@naver.com>
2024-01-03 11:02:23 +0900

    11:02

--- src/main/java/com/study/ch05/If02.java
+++ src/main/java/com/study/ch05/If02.java
@@ -3,14 +3,14 @@ package com.study.ch05;
 public class If02 {
     public static void main(String[] args) {
         int x = 1;
-        int y = 0;
+        int y = 1;
 
         if(x == 0 && y == 0) System.out.println("원점");
         else if (x > 0 && y > 0) System.out.println("1사분면");
         else if (x > 0 && y < 0) System.out.println("2사분면");
         else if (x < 0 && y > 0) System.out.println("3사분면");
         else if (x < 0 && y < 0) System.out.println("4사분면");
-        else System.out.println("오류");
+        else System.out.println("없음");
 
 //        if(x == 0 || y == 0) {
 //            System.out.println("오류");
