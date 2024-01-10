c7f043770fb26b43c2d52013f0156bab30687912
sanghyun27 <rhffoa207@naver.com>
2024-01-10 11:04:22 +0900

    33

--- src/main/java/com/study/ch14/StudentMain.java
+++ src/main/java/com/study/ch14/StudentMain.java
@@ -4,8 +4,5 @@ public class StudentMain {
     public static void main(String[] args) {
         Student student = new Student("김준일", "01099881916", "동래구", 31);
         System.out.println(student);
-
-
     }
-
 }
